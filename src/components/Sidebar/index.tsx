7c54bd5230bd53ac035b28ad65605f1643a9755f
mikera <mike.silva@pixter.com.br>
2023-08-19 20:37:14 -0300

    Widget de Espaço Usado

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -1,10 +1,19 @@
-import { Search } from 'lucide-react'
+import {
+  BarChart2,
+  CheckSquare,
+  Flag,
+  Home,
+  Layers,
+  Search,
+  Users,
+} from 'lucide-react'
 import { Logo } from './Logo'
-import { MainNavigation } from './MainNavigation'
+import { NavItem } from './NavItem'
+import { UsedSpaceWidget } from './UsedSpaceWidget'
 
 export function Sidebar() {
   return (
-    <aside className="space-y-6 border-r-2 border-r-zinc-200 px-5 py-8">
+    <aside className="flex flex-col gap-6 border-r-2 border-r-zinc-200 px-5 py-8">
       <Logo />
 
       <div className="mx-2 flex w-full items-center gap-2 rounded-lg border border-zinc-300 px-3 py-2 shadow-sm">
@@ -15,7 +24,16 @@ export function Sidebar() {
         />
       </div>
 
-      <MainNavigation />
+      <nav aria-label="primaria" className="list-none space-y-0.5">
+        <NavItem title="Home" icon={Home} />
+        <NavItem title="Dashboard" icon={BarChart2} />
+        <NavItem title="Projects" icon={Layers} />
+        <NavItem title="Tasks" icon={CheckSquare} />
+        <NavItem title="Reporting" icon={Flag} />
+        <NavItem title="Users" icon={Users} />
+      </nav>
+
+      <UsedSpaceWidget />
     </aside>
   )
 }

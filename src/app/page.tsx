8cc0e5aad501b4aaf3d04683316383441b71a3be
mikera <mike.silva@pixter.com.br>
2023-08-21 10:28:06 -0300

    Criando Abas com Radix Tabs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,10 @@
+import { SettingsTabs } from '@/components/SettingsTabs'
+
 export default function Home() {
-  return <h1 className="">conteúdo</h1>
+  return (
+    <>
+      <h1 className="mb-6 ml-8 text-3xl font-medium text-zinc-900">Settings</h1>
+      <SettingsTabs />
+    </>
+  )
 }
